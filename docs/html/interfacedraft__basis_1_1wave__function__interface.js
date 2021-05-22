2ffc5dde0fbb4feaa1367213e4ca8473b692a8f1
Steven Tseng <45262642+steventsengXD@users.noreply.github.com>
2021-05-23 00:50:19 +0100

    Delete docs/html directory

--- docs/html/interfacedraft__basis_1_1wave__function__interface.js
+++
@@ -1,4 +0,0 @@
-var interfacedraft__basis_1_1wave__function__interface =
-[
-    [ "wave_function_interface", "interfacedraft__basis_1_1wave__function__interface.html#a196281b351ff121880cfbb1b2796d795", null ]
-];
