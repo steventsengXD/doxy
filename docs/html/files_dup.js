ea508508870b699679ddd7e19eea145d8b7d821a
Steven Tseng <steven.tseng@warwick.ac.uk>
2021-05-23 00:43:07 +0100

    tutorial

---
+++ docs/html/files_dup.js
@@ -0,0 +1,20 @@
+var files_dup =
+[
+    [ "basis_example.f90", "basis__example_8f90.html", "basis__example_8f90" ],
+    [ "basis_functions.f90", "basis__functions_8f90.html", "basis__functions_8f90" ],
+    [ "commonblock.f90", "commonblock_8f90.html", "commonblock_8f90" ],
+    [ "component_functions.f90", "component__functions_8f90.html", "component__functions_8f90" ],
+    [ "constants.f90", "constants_8f90.html", "constants_8f90" ],
+    [ "draft_basis.f90", "draft__basis_8f90.html", "draft__basis_8f90" ],
+    [ "driver.f90", "driver_8f90.html", "driver_8f90" ],
+    [ "init.f90", "init_8f90.html", "init_8f90" ],
+    [ "latin_driver.f90", "latin__driver_8f90.html", "latin__driver_8f90" ],
+    [ "loop_test.f90", "loop__test_8f90.html", "loop__test_8f90" ],
+    [ "loop_test_update.f90", "loop__test__update_8f90.html", "loop__test__update_8f90" ],
+    [ "MCMC.f90", "MCMC_8f90.html", "MCMC_8f90" ],
+    [ "MCMC_test_driver.f90", "MCMC__test__driver_8f90.html", "MCMC__test__driver_8f90" ],
+    [ "param_search.f90", "param__search_8f90.html", "param__search_8f90" ],
+    [ "readtxt.f90", "readtxt_8f90.html", "readtxt_8f90" ],
+    [ "wavefunc_results.f90", "wavefunc__results_8f90.html", "wavefunc__results_8f90" ],
+    [ "write_file.f90", "write__file_8f90.html", "write__file_8f90" ]
+];
