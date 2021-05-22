38095610d9c71e29721e6090a76a5bbfb8542c72
Steven Tseng <steven.tseng@warwick.ac.uk>
2021-05-22 23:05:14 +0100

    new

--- docs/html/tutorial_page.js
+++ docs/html/tutorial_page.js
@@ -1,5 +1,6 @@
 var tutorial_page =
 [
     [ "Running the code", "startup.html", null ],
-    [ "Plot code using this function", "output.html", null ]
+    [ "Plot code using this function", "output.html", null ],
+    [ "User can choose/change these options", "options.html", null ]
 ];
