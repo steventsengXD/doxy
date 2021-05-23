4c7a072d41599099dec78215d076aba5fc69c3a1
Steven Tseng <steven.tseng@warwick.ac.uk>
2021-05-23 04:45:39 +0100

    added python docs

---
+++ docs/docs/PX915__plotting_8py.js
@@ -0,0 +1,16 @@
+var PX915__plotting_8py =
+[
+    [ "ax", "PX915__plotting_8py.html#a56bba015ed4ca1b564c13053f5db0c14", null ],
+    [ "c", "PX915__plotting_8py.html#ac012df362907d0d48d1b41e497d54cf3", null ],
+    [ "cmap", "PX915__plotting_8py.html#a70681b49d0745863b9650fce2d2daeb1", null ],
+    [ "coords", "PX915__plotting_8py.html#a0b3f1158725aef2dc1b5f099f027c312", null ],
+    [ "dof", "PX915__plotting_8py.html#acebaaa697f52031efb75f324f7bf4380", null ],
+    [ "fig", "PX915__plotting_8py.html#a40b7fddc12d03d92964bb5fe26ad551a", null ],
+    [ "file", "PX915__plotting_8py.html#aceba4f6792f2ab5e052fee382cf67784", null ],
+    [ "hamiltonian", "PX915__plotting_8py.html#aea9470a5364e7888c9f444376371e443", null ],
+    [ "linewidth", "PX915__plotting_8py.html#a5b7fb324c15bfa87593ed28b63025ee8", null ],
+    [ "wavefunction", "PX915__plotting_8py.html#a0d521b532a96d27e8e761e595c75ae90", null ],
+    [ "xdata", "PX915__plotting_8py.html#a5b648ee1dc265c6d4685b49e6116e032", null ],
+    [ "ydata", "PX915__plotting_8py.html#acef0fc2a710a36b53e92aedac6c8c067", null ],
+    [ "zdata", "PX915__plotting_8py.html#a2cf33dc0c63a5b5596e27f1ebaade9bc", null ]
+];
