848d4125ea90aad128cf232318bc3f821857de1d
Steven Tseng <steven.tseng@warwick.ac.uk>
2021-05-23 04:00:40 +0100

    removed python docs, will do those separately

--- docs/docs/commonblock_8py.js
+++
@@ -1,7 +0,0 @@
-var commonblock_8py =
-[
-    [ "update", "commonblock_8py.html#adea55158088e69261e82ac815e20d1c4", null ],
-    [ "a", "commonblock_8py.html#a2c7c43f45c305d23046ca9a88f4b7c2e", null ],
-    [ "area", "commonblock_8py.html#a8e43d8d815ec477c46d8ee53db6c4954", null ],
-    [ "l", "commonblock_8py.html#a13fe31bdad63726b75eef087c1a0e020", null ]
-];
