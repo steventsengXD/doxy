decc07906cbe5bd3b1702bfdf00acb3e6963ca56
Steven Tseng <steven.tseng@warwick.ac.uk>
2021-05-25 15:16:39 +0100

    updated docs

--- docs/docs/navtreedata.js
+++ docs/docs/navtreedata.js
@@ -24,13 +24,8 @@ var NAVTREE =
 [
   [ "PX 915 Project Title/Name Here", "index.html", [
     [ "PX 915 Group B Documentation", "index.html", [
-      [ "Introduction", "index.html#Introduction", null ],
-      [ "This document describes the source code.", "index.html#autotoc_md0", null ],
-      [ "Can place image below, maybe a diagram of the code", "index.html#autotoc_md1", null ],
-      [ "<div class=\"dyncontent\">", "index.html#autotoc_md2", null ],
-      [ "<div class=\"center\"><img src=\"latin__driver_8f90_a9c41b02ba03edb8de4ce1751ea3cf05f_cgraph.png\" border=\"0\" # usemap=\"#latin__driver_8f90_a9c41b02ba03edb8de4ce1751ea3cf05f_cgraph\" alt=\"\"/></div>", "index.html#autotoc_md3", null ],
-      [ "</div>", "index.html#autotoc_md4", [
-        [ "Input", "index.html#Data", null ]
+      [ "Introduction", "index.html#Introduction", [
+        [ "Data Input", "index.html#data_input", null ]
       ] ]
     ] ],
     [ "Tutorial", "tutorial_page.html", "tutorial_page" ],
