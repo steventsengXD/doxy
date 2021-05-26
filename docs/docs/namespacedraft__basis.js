bd4b15e89bd362ae6cc59eb706bde73bb0bf0741
Steven Tseng <steven.tseng@warwick.ac.uk>
2021-05-26 05:41:40 +0100

    docs

--- docs/docs/namespacedraft__basis.js
+++
@@ -1,4 +0,0 @@
-var namespacedraft__basis =
-[
-    [ "wave_function_interface", "interfacedraft__basis_1_1wave__function__interface.html", "interfacedraft__basis_1_1wave__function__interface" ]
-];
--- docs/docs/readtxt_8f90.js
+++
@@ -1,4 +0,0 @@
-var readtxt_8f90 =
-[
-    [ "readtxt", "readtxt_8f90.html#a0258e1e2955818c8541756294e701af9", null ]
-];
