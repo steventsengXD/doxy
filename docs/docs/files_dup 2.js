994fa83a46f94deb2c7edae7a7897413b339db07
Steven Tseng <steven.tseng@warwick.ac.uk>
2021-05-26 15:55:27 +0100

    more docs

---
+++ docs/docs/files_dup 2.js
@@ -0,0 +1,15 @@
+var files_dup =
+[
+    [ "basis_functions.f90", "basis__functions_8f90.html", "basis__functions_8f90" ],
+    [ "calc.f90", "calc_8f90.html", "calc_8f90" ],
+    [ "component_functions.f90", "component__functions_8f90.html", "component__functions_8f90" ],
+    [ "constants.f90", "constants_8f90.html", "constants_8f90" ],
+    [ "electron_density.f90", "electron__density_8f90.html", "electron__density_8f90" ],
+    [ "init_params.py", "init__params_8py.html", "init__params_8py" ],
+    [ "latin_driver.f90", "latin__driver_8f90.html", "latin__driver_8f90" ],
+    [ "MCMC.f90", "MCMC_8f90.html", "MCMC_8f90" ],
+    [ "netcdf_file.f90", "netcdf__file_8f90.html", "netcdf__file_8f90" ],
+    [ "param_search.f90", "param__search_8f90.html", "param__search_8f90" ],
+    [ "plotting.py", "plotting_8py.html", "plotting_8py" ],
+    [ "read_data.f90", "read__data_8f90.html", "read__data_8f90" ]
+];
